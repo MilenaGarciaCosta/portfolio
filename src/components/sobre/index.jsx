d78c9dd4cf0ed700289e3ccd0b1e897df69e86d0
Milena Garcia <109882477+MilenaGarciaCosta@users.noreply.github.com>
2024-02-24 16:58:43 -0300

    Update index.jsx

--- src/components/sobre/index.jsx
+++ src/components/sobre/index.jsx
@@ -5,7 +5,7 @@ export const Apresentacao = () => {
         <div>
             <section class='section-base sobre'>
                 <section class='texto-borda'>
-                    <p>&emsp; Olá mundo! Meu nome é <strong>Milena</strong>, sou técnica em desenvolvimento de sistemas e estudante de engenharia de software na FIAP. Minha paixão pela tecnologia, junto com sua constante evolução, aumenta minha vontade de aprender novas coisas para melhorar a eficiência e qualidade de tudo que faço.</p>
+                    <p>&emsp; Olá mundo! Meu nome é <strong>Milena</strong>, sou técnica em desenvolvimento de sistemas e estudante de engenharia de software na <strong>FIAP</strong>. Minha paixão pela tecnologia, junto com sua constante evolução, aumenta minha vontade de aprender novas coisas para melhorar a eficiência e qualidade de tudo que faço.</p>
                     <p>&emsp; Meus objetivos são estudar cada vez mais para me tornar uma <strong>Desenvolvedora Full-Stack</strong>.</p>
                 </section>
                 <img src="img/foto-perfil-bolhas.svg" alt="foto de perfil" height='330px' />
